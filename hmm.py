23df6e0064787a662d6e375a77134184e9dd2230
Munender0 <118874679+Munender0@users.noreply.github.com>
2022-12-06 11:03:17 +0530

    Update hmm.py

--- hmm.py
+++ hmm.py
@@ -1,11 +1,19 @@
-import cmath
-a=2
-b=10
-c=12
-d=(b**2)-(4*a*c)
+code = input('Enter the code: ')
+if code == "#a":
 
-root1=(-b-cmath.sqrt(d))/2*a
-root2=(-b+cmath.sqrt(d))/2*a
-print('This is the square root: ')
-print(root1)
-print(root2)
+  print('Hello bro', code)
+
+  print('Put your quatratic equation herE sIR')
+  import cmath
+  a=2
+  b=10
+  c=12
+  d=(b**2)-(4*a*c)
+
+  root1=(-b-cmath.sqrt(d))/2*a
+  root2=(-b+cmath.sqrt(d))/2*a
+  print('This is the square root: ')
+  print(root1)
+  print(root2)
+else: 
+    print('You Are Hacked/')
